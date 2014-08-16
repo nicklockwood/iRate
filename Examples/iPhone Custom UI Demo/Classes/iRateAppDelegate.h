4a20c30d4b55dd92b0c9b04879602a1f8fc56266
Nick Lockwood <nick@charcoaldesign.co.uk>
2014-08-15 17:35:10 -0700

    Added custom UI example

---
+++ Examples/iPhone Custom UI Demo/Classes/iRateAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  iRateAppDelegate.h
+//  iRate
+//
+//  Created by Nick Lockwood on 26/01/2011.
+//  Copyright 2011 Charcoal Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iRateAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, strong) IBOutlet UIWindow *window;
+
+@end
+
