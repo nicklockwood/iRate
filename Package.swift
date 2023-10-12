18cc3c878e916a1005333315c064ccf39d8878eb
dong2810 <53572317+dong2810@users.noreply.github.com>
2023-10-12 11:30:29 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
 		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
 			name: "iRate",
-			dependencies: []),
+			dependencies: [],
+			path: "Sources/iRate"),
 	]
 )
