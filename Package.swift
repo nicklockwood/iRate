8152a9754e101c6bc49d3faa9766f0fe44d87a32
dong2810 <53572317+dong2810@users.noreply.github.com>
2023-10-12 14:51:31 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,6 @@ let package = Package(
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
-			name: "iRate",
-			dependencies: []),
+			name: "iRate"),  
 	]
 )
