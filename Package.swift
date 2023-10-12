3a427f3f6eed79c3bfdadb5d7c5c87d3522e9026
Dong <dong2810123@gmail.com>
2023-10-12 16:50:50 +0700

    ađ

--- Package.swift
+++ Package.swift
@@ -1,26 +1,32 @@
-// swift-tools-version:5.7.1
+// swift-tools-version:5.5
+
 import PackageDescription
 
 let package = Package(
 	name: "iRate",
 	platforms: [
-		.iOS(.v13), // Adjust as needed
+		.iOS(.v13),
 	],
 	products: [
-		// Products define the executables and libraries produced by a package, and make them visible to other packages.
 		.library(
 			name: "iRate",
-			targets: ["iRate"]),
+			targets: ["iRate"]
+		),
 	],
 	dependencies: [
-		// Dependencies declare other packages that this package depends on.
-		// .package(url: /* package url */, from: "1.0.0"),
+		.package(url: "https://github.com/dong2810/iRate.git", from: "0.4.0"),
 	],
 	targets: [
-		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
-		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
 			name: "iRate",
-			dependencies: []),
+			dependencies: [
+				"iRate",
+			],
+			path: "Code"
+		),
+		.testTarget(
+			name: "iRate",
+			dependencies: ["iRate"]
+		),
 	]
 )
