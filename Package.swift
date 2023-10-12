fd226b96dc3d3cb7eb6edefce15d3d05cc0b6ea9
dong2810 <53572317+dong2810@users.noreply.github.com>
2023-10-12 17:15:18 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,16 @@
-// swift-tools-version:5.5
-
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
-	name: "iRate",
-	platforms: [
-		.iOS(.v13),
-	],
-	products: [
-		.library(
-			name: "iRate",
-			targets: ["iRate"]
-		),
-	],
-	dependencies: [
-		.package(url: "https://github.com/dong2810/iRate.git", from: "0.4.0"),
-	],
-	targets: [
-		.target(
-			name: "iRate",
-		)
-	]
+    name: "iRate",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "iRate",
+            targets: ["iRate"]),
+    ],
+    targets: [
+        .target(
+            name: "iRate")
+    ]
 )
